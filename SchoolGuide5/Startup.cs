000ee8d5571b35883eb4ff0503197b607911c48a
RoaaKhatab <RoaaKhatab@users.noreply.github.com>
2019-05-11 09:17:32 +0200

    Add project files.

---
+++ SchoolGuide5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolGuide5.Startup))]
+namespace SchoolGuide5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
